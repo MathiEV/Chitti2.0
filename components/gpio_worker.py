56e30c262b5b731cfdcc5308073277af7ebc0d77
Sabarish Vijayakumar <vijayakumar.sabarish2@in.bosch.com>
2023-03-26 16:44:50 +0100

    Correction on the GPIO and Servo workers   1. Rename the workers and its respective test files   2. Restructure the worker test folder
    Signed-off-by: Sabarish Vijayakumar <vijayakumar.sabarish2@in.bosch.com>

--- components/gpio_worker.py
+++ components/gpio_worker.py
@@ -4,7 +4,7 @@
 ##################################################
 import RPi.GPIO as GPIO
 
-class GPIO_Manager:
+class GPIO_Worker:
     _def_pin_config = {
         5:GPIO.OUT,
         6:GPIO.OUT,    
@@ -15,18 +15,18 @@ class GPIO_Manager:
         23:GPIO.OUT,
         24:GPIO.OUT,
         26:GPIO.OUT,
-        25:GPIO.OUT
-        
+        25:GPIO.OUT        
         }
+        
+    def __init__(self):
+        print("GPIO worker init started")
+        GPIO.setmode(GPIO.BCM)        
+        for pin, mode in  self._def_pin_config.items():            
+            GPIO.setup(pin, mode)        
+        print("GPIO worker init completed")
+        
     def set_gpio(self, pin, state):
         GPIO.output(pin,state)
     
     def get_gpio(self, pin):
         return GPIO.input(pin)
-        
-    def __init__(self):
-        print("Init started")
-        GPIO.setmode(GPIO.BCM)        
-        for pin, mode in  self._def_pin_config.items():            
-            GPIO.setup(pin, mode)        
-        print("Init completed")
--- components/servo_worker.py
+++ components/servo_worker.py
@@ -4,7 +4,7 @@
 ##################################################
 from adafruit_servokit import ServoKit
 
-class Rotation_Manager:
+class Servo_Worker:
    
     #ZeroOffset is the intial value/position required for the servo motor
     
@@ -13,10 +13,12 @@ class Rotation_Manager:
     _actuationRange = 180
 
     def __init__(self, input_channel):
+        print("Servo worker init started")
         self.channel = input_channel            
         self._kit.servo[self.channel].actuation_range = self._actuationRange
         #Modify the pulse width range based on the servo
-        self._kit.servo[self.channel].set_pulse_width_range(0, 2520) 
+        self._kit.servo[self.channel].set_pulse_width_range(0, 2520)
+        print("Servo worker init completed")
 
     def setAngle(self, angle):
         self._kit.servo[self.channel].angle = angle
