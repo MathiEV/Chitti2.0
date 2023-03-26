81839e046d3f72cf13abd1aa9f7e5e1f6aeaf95e
MathiEV <electrovisiontechnology@gmail.com>
2023-03-26 19:52:22 +0530

    Enabled ModeMonitor class.

---
+++ System/mode_monitor.py
@@ -0,0 +1,10 @@
+##################################################
+# AUTHOR                                         #
+# MATHIYAZHAGAN RAMANISELVA                      #
+##################################################
+
+class ModeMonitor:
+    def __init__(self):
+        print("ModeMonitor Init Started")
+
+        print("ModeMonitor Init Completed")
