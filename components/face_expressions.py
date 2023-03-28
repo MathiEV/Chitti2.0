d4891db114f5b162dbc466db5ce15f546ee4e987
krushnakumar1 <128345152+krushnakumar1@users.noreply.github.com>
2023-03-28 09:59:10 +0530

    Update face_expressions.py
    fixed errors

--- components/face_expressions.py
+++ components/face_expressions.py
@@ -5,7 +5,7 @@
 
 
 from adafruit_servokit import ServoKit
-from Servo_Worker import Servo_Worker
+from components.servo_worker import Servo_Worker
 
 #Class to set expressions to face using servo motors
 _LEFT_EYE = 1
@@ -15,54 +15,53 @@ _RIGHT_EYE_BROW = 4
 _MOUTH = 5
 
 #Angle macro
-_SMILE_LEFT_EYE = 0
-_SMILE_RIGHT_EYE = 0
-_SMILE_LEFT_EYE_BROW = 0
-_SMILE_RIGHT_EYE_BROW = 0
-_SMILE_MOUTH = 0
+_SMILE_LEFT_EYE = 90
+_SMILE_RIGHT_EYE = 90
+_SMILE_LEFT_EYE_BROW = 90
+_SMILE_RIGHT_EYE_BROW = 90
+_SMILE_MOUTH = 90
 
-_SAD_LEFT_EYE = 0
-_SAD_RIGHT_EYE = 0
-_SAD_LEFT_EYE_BROW = 0
-_SAD_RIGHT_EYE_BROW = 0
-_SAD_MOUTH = 0
+_SAD_LEFT_EYE = 90
+_SAD_RIGHT_EYE = 90
+_SAD_LEFT_EYE_BROW = 90
+_SAD_RIGHT_EYE_BROW = 90
+_SAD_MOUTH = 90
 
-_ANGRY_LEFT_EYE = 0
-_ANGRY_RIGHT_EYE = 0
-_ANGRY_LEFT_EYE_BROW = 0
-_ANGRY_RIGHT_EYE_BROW = 0
-_ANGRY_MOUTH = 0
+_ANGRY_LEFT_EYE = 90
+_ANGRY_RIGHT_EYE = 90
+_ANGRY_LEFT_EYE_BROW = 90
+_ANGRY_RIGHT_EYE_BROW = 90
+_ANGRY_MOUTH = 90
 
 class Face_Expressions:
    
   
     def __init__(self):
-        
-        lefteye = Servo_Worker(_LEFT_EYE)
-        righteye = Servo_Worker(_LEFT_EYE)
-        mouth = Servo_Worker(_MOUTH)
-        lefteye_brow = Servo_Worker(_LEFT_EYE_BROW)
-        righteye_brow = Servo_Worker(_RIGHT_EYE_BROW)            
+        self.lefteye = Servo_Worker(_LEFT_EYE)
+        self.righteye = Servo_Worker(_LEFT_EYE)
+        self.mouth = Servo_Worker(_MOUTH)
+        self.lefteye_brow = Servo_Worker(_LEFT_EYE_BROW)
+        self.righteye_brow = Servo_Worker(_RIGHT_EYE_BROW)            
 
     def SetExpression_Happy(self):
-        lefteye.setAngle(_SMILE_LEFT_EYE)
-        righteye.setAngle(_SMILE_RIGHT_EYE)
-        mouth.setAngle(_SMILE_LEFT_EYE_BROW)
-        lefteye_brow.setAngle(_SMILE_RIGHT_EYE_BROW)
-        righteye_brow.setAngle(_SMILE_MOUTH)
+        self.lefteye.set_angle(_SMILE_LEFT_EYE)
+        self.righteye.set_angle(_SMILE_RIGHT_EYE)
+        self.mouth.set_angle(_SMILE_LEFT_EYE_BROW)
+        self.lefteye_brow.set_angle(_SMILE_RIGHT_EYE_BROW)
+        self.righteye_brow.set_angle(_SMILE_MOUTH)
     
     
     def SetExpression_Sad(self):
-        lefteye.setAngle(_SAD_LEFT_EYE)
-        righteye.setAngle(_SAD_RIGHT_EYE)
-        mouth.setAngle(_SAD_LEFT_EYE_BROW)
-        lefteye_brow.setAngle(_SAD_RIGHT_EYE_BROW)
-        righteye_brow.setAngle(_SAD_MOUTH)
+        self.lefteye.set_angle(_SAD_LEFT_EYE)
+        self.righteye.set_angle(_SAD_RIGHT_EYE)
+        self.mouth.set_angle(_SAD_LEFT_EYE_BROW)
+        self.lefteye_brow.set_angle(_SAD_RIGHT_EYE_BROW)
+        self.righteye_brow.set_angle(_SAD_MOUTH)
         
-     def SetExpression_Angry(self):
-        lefteye.setAngle(_ANGRY_LEFT_EYE)
-        righteye.setAngle(_ANGRY_RIGHT_EYE)
-        mouth.setAngle(_ANGRY_LEFT_EYE_BROW)
-        lefteye_brow.setAngle(_ANGRY_RIGHT_EYE_BROW)
-        righteye_brow.setAngle(_ANGRY_MOUTH)
+    def SetExpression_Angry(self):
+        self.lefteye.set_angle(_ANGRY_LEFT_EYE)
+        self.righteye.set_angle(_ANGRY_RIGHT_EYE)
+        self.mouth.set_angle(_ANGRY_LEFT_EYE_BROW)
+        self.lefteye_brow.set_angle(_ANGRY_RIGHT_EYE_BROW)
+        self.righteye_brow.set_angle(_ANGRY_MOUTH)
     
