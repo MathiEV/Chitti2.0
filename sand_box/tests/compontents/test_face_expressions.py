4c07f0892159c40eb86166b7444548bad4ec3d89
krushnakumar1 <128345152+krushnakumar1@users.noreply.github.com>
2023-03-28 09:58:02 +0530

    Create test_face_expressions.py
    test code for face expressions

---
+++ sand_box/tests/compontents/test_face_expressions.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+@Author: Krushna kuamr 
+@Date: 26.03.2023
+"""
+import signal
+import sys
+import os
+import time
+sys.path.append('../../../')
+from datetime import datetime
+from components.face_expressions import Face_Expressions
+from system.mode_monitor import Mode_Monitor
+from system.alert_manager import Alert_Manager
+from application_modules.logger_module import Logger_Module
+from application_modules.path_module import Path_Module
+
+logger = Logger_Module()
+
+def signal_handler(_SIGNO, _STACK_FRAME):
+    logger.logDebug("Signal Received : Exiting...")
+    sys.exit(0)
+    
+  
+
+def main():
+	face_expression = Face_Expressions()
+	face_expression.SetExpression_Happy()
+	time.sleep(2)
+	
+
+
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
+    main()
