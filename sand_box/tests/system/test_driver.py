2c96bfc3576c6abbabd4a848cb5517f6d5050887
krushnakumar1 <128345152+krushnakumar1@users.noreply.github.com>
2023-03-28 21:08:49 +0530

    Update test_driver.py
    tested using mode

--- sand_box/tests/system/test_driver.py
+++ sand_box/tests/system/test_driver.py
@@ -5,11 +5,16 @@ import time
 sys.path.append('../../../')
 from system.driver import Driver
 from components.gpio_worker import GPIO_Worker
+from system.mode_monitor import Mode_Monitor
+from system.alert_manager import Alert_Manager
+import common.common_definitions as comm_def
 
 _MOVE_TIMER = 1
 
 gpio = GPIO_Worker()
-driver = Driver()
+mode_monitor = Mode_Monitor()
+#alert_manager = Alert_Manager(mode_monitor.update_mode)
+driver = Driver(mode_monitor)
 
 def signal_handler(_SIGNO, _STACK_FRAME):
     gpio.clean_up()
@@ -22,4 +27,15 @@ def drive_motor():
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal_handler)
     signal.signal(signal.SIGTERM, signal_handler)
-    drive_motor()
+           
+    mode_monitor.update_mode(comm_def._GUEST_MODE)
+    time.sleep(10)
+    mode_monitor.update_mode(comm_def._FUN_MODE)
+    time.sleep(5)
+    mode_monitor.update_mode(comm_def._SECURITY_MODE)
+    time.sleep(2)
+    mode_monitor.update_mode(comm_def._FUN_MODE)
+    time.sleep(5)
+    
+    #drive_motor()
+motor()
