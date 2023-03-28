0b07750b93e520d912a85ade482a0c46091d13bd
krushnakumar1 <128345152+krushnakumar1@users.noreply.github.com>
2023-03-28 17:53:59 +0530

    Update face_expressions.py
    Pan and tilt functionalities are added

--- components/face_expressions.py
+++ components/face_expressions.py
@@ -23,6 +23,10 @@ _LEFT_EYE_BROW = 11
 _RIGHT_EYE_BROW = 12
 _MOUTH = 15
 
+#PanTilt configuration
+_PAN = 1
+_TILT = 0
+
 #Angle macro
 _SMILE_LEFT_EYE = 90
 _SMILE_RIGHT_EYE = 90
@@ -32,8 +36,8 @@ _SMILE_MOUTH = 80
 
 _SAD_LEFT_EYE = 90
 _SAD_RIGHT_EYE = 83
-_SAD_LEFT_EYE_BROW = 90
-_SAD_RIGHT_EYE_BROW = 90
+_SAD_LEFT_EYE_BROW = 88
+_SAD_RIGHT_EYE_BROW = 92
 _SAD_MOUTH = 65
 
 _ANGRY_LEFT_EYE = 90
@@ -48,10 +52,19 @@ _DEFAULT_LEFT_EYE_BROW = 98
 _DEFAULT_RIGHT_EYE_BROW = 82
 _DEFAULT_MOUTH = 65
 
+#Angle for pantilt
+_DEFAULT_PAN = 110
+_DEFAULT_TILT = 105
 
+_SAD_PAN = 90
 
 class Face_Expressions:
    
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(Face_Expressions, cls).__new__(cls)
+        return cls.instance
+
   
     def __init__(self):
         self.lefteye = Servo_Worker(_LEFT_EYE)
@@ -59,7 +72,9 @@ class Face_Expressions:
         self.mouth = Servo_Worker(_MOUTH)
         self.lefteye_brow = Servo_Worker(_LEFT_EYE_BROW)
         self.righteye_brow = Servo_Worker(_RIGHT_EYE_BROW)       
-        self.set_expression_default()     
+        self.pan = Servo_Worker(_PAN)
+        self.tilt = Servo_Worker(_TILT)
+        self.set_expression_default()
         
 
     def set_expression_smile(self):
@@ -68,6 +83,7 @@ class Face_Expressions:
         self.mouth.set_angle(_SMILE_MOUTH)
         self.lefteye_brow.set_angle(_SMILE_LEFT_EYE_BROW)
         self.righteye_brow.set_angle(_SMILE_RIGHT_EYE_BROW)
+        self.pan.set_angle(_DEFAULT_PAN)
             
     
     def set_expression_sad(self):
@@ -76,6 +92,7 @@ class Face_Expressions:
         self.mouth.set_angle(_SAD_MOUTH)
         self.lefteye_brow.set_angle(_SAD_LEFT_EYE_BROW)
         self.righteye_brow.set_angle(_SAD_RIGHT_EYE_BROW)
+        self.pan.set_angle(_SAD_PAN)
         
     def set_expression_angry(self):
         #self.lefteye.set_angle(_ANGRY_LEFT_EYE)
@@ -83,6 +100,7 @@ class Face_Expressions:
         self.mouth.set_angle(_ANGRY_MOUTH)
         self.lefteye_brow.set_angle(_ANGRY_LEFT_EYE_BROW)
         self.righteye_brow.set_angle(_ANGRY_RIGHT_EYE_BROW)
+        self.pan.set_angle(_DEFAULT_PAN)
 
     def set_expression_default(self):
         #self.lefteye.set_angle(_DEFAULT_LEFT_EYE)
@@ -90,5 +108,19 @@ class Face_Expressions:
         self.mouth.set_angle(_DEFAULT_MOUTH)
         self.lefteye_brow.set_angle(_DEFAULT_LEFT_EYE_BROW)
         self.righteye_brow.set_angle(_DEFAULT_RIGHT_EYE_BROW)
+        self.pan.set_angle(_DEFAULT_PAN)
+        self.tilt.set_angle(_DEFAULT_TILT)
     
-    
+    def set_pan_angle(self, angle): 
+        if angle >= 85 and angle <= 140:
+            self.pan.set_angle(angle)
+            self.last_pan_angle = angle
+        else:
+            self.pan.set_angle(_DEFAULT_PAN)
+            
+           
+    def set_tilt_angle(self, angle): 
+        if angle >= 75 and angle <= 135:
+            self.tilt.set_angle(angle)
+        else:
+            self.tilt.set_angle(_DEFAULT_TILT)
